db289fc9d56b76079945f4a3e9f59b5add4f10da
anvar89 <khalitoff@live.ru>
2021-03-11 19:23:41 +0500

    Рефакторинг2

---
+++ Lesson4/Task2/InitialData/ITree.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2
+{
+    public interface ITree
+    {
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
+    }
+
+}
---
+++ Lesson4/Task2/InitialData/NodeInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Task2
+{
+    public class NodeInfo
+    {
+        public int Depth { get; set; }
+        public TreeNode Node { get; set; }
+    }
+
+}
